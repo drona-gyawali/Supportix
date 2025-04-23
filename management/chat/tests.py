1a30a8c8e7e3dcad9c0614dd80ed81c187141453
drona-gyawali <dronarajgyawali@gmail.com>
2025-04-23 21:54:47 +0545

    [FEATURE] Database migration to postgres

--- management/chat/tests.py
+++ management/chat/tests.py
@@ -1,14 +1,14 @@
 from rest_framework.test import APITestCase
-from core.models import *
+from django.contrib.auth.models import User
 from rest_framework import status
 
 class ApiTest(APITestCase):
-    fixtures= ['main.json']
-    def setup():
-        print('setup called')
-    # api/chat/view
+    fixtures = ['main.json']
+
+    def setUp(self):
+        self.user = User.objects.create_user(username="admin", password="admin123")
+        self.client.login(username="admin", password="admin123")
+
     def test_get_chat_history(self):
-        _response = self.client.get('/api/chat/view', format='json')
-        _data = _response.json()
-        print(_data)
-        self.assertEqual(_response.status_code,status.HTTP_200_OK)
+        response = self.client.get('/api/chat/view', format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- management/main/settings.py
+++ management/main/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv
 from pathlib import Path
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -41,10 +43,8 @@ INSTALLED_APPS = [
     "rest_framework",
 ]
 EXTERNAL_APPS = [
-    "pika",
     "chat",
     "core",
-    "Notification",
     "channels",
 ]
 INSTALLED_APPS += EXTERNAL_APPS
@@ -101,9 +101,13 @@ ALLOWED_HOSTS = ["127.0.0.1", "8000"]
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
