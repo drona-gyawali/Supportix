a65cbb2585d4b8fcab09e64da09c73a338298b91
drona-gyawali <dronarajgyawali@gmail.com>
2025-05-08 19:07:12 +0545

    Update redis config

--- management/main/settings.py
+++ management/main/settings.py
@@ -157,6 +157,9 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
+REDIS_PORT = os.getenv("REDIS_PORT", "6379")
+
 # Celery Configuration
 CELERY_BROKER_URL = "redis://localhost:6379/0"
 CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
