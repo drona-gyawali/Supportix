faf52aeae21e0aa48e2d9e6ff92c926c2f975098
drona-gyawali <dronarajgyawali@gmail.com>
2025-05-25 18:06:02 +0545

    Disscusion Section Updated Model
    Signed-off-by: drona-gyawali <dronarajgyawali@gmail.com>

--- management/chat/models.py
+++ management/chat/models.py
@@ -1,7 +1,5 @@
-from django.conf import settings
 from django.db import models
-
-# Need to add some field such as link, file, image etc... : Future Work...
+from core.models import User
 
 
 class ChatGroup(models.Model):
@@ -10,12 +8,11 @@ class ChatGroup(models.Model):
     def __str__(self):
         return self.group_name
 
-
 class GroupMessage(models.Model):
     group = models.ForeignKey(
         ChatGroup, related_name="chat_messages", on_delete=models.CASCADE
     )
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="messages")
     parent = models.ForeignKey(
         "self",
         on_delete=models.CASCADE,
@@ -33,3 +30,27 @@ class GroupMessage(models.Model):
 
     def __str__(self):
         return f"{self.author.username}: {self.body}"
+
+class Attachement(models.Model):
+    discussion= models.ForeignKey(GroupMessage, on_delete=models.CASCADE, related_name = 'attachment')
+    chat_image = models.ImageField(upload_to="chat_image/")
+    file_image = models.ImageField(upload_to='attachment/')
+    file_name = models.CharField(max_length=30, blank=True)
+    link = models.URLField(blank=True)
+    reaction = models.CharField(max_length=50,blank=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return (
+            f'{{'
+            f'"discussion": "{self.discussion.author.username}", '
+            f'"chat_image": "{self.chat_image.url if self.chat_image else ""}", '
+            f'"file_image": "{self.file_image.url if self.file_image else ""}", '
+            f'"file_name": "{self.file_name}", '
+            f'"link": "{self.link}", '
+            f'"reaction": "{self.reaction}", '
+            f'"uploaded_at": "{self.uploaded_at}", '
+            f'"updated_at": "{self.updated_at}"'
+            f'}}'
+        )
